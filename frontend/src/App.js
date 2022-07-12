27e2024959e559392de32a4d784487301ae87f5f
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-07-12 16:37:53 +0100

    Figuring how to connect rails and react
    Why:
    * Testing how to solve connection between frontend and backend
    
    How:
    * Simulating the articles table in the file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Signup from "./pages/Signup";
 import WheelPage from "./pages/Wheel";
 import SocialNetworks from "./pages/SocialNetworks";
 import WinPrize from "./pages/WinPrize";
+import Articles from "./pages/Articles";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/wheel" element={<WheelPage />} />
           <Route path="/social-network" element={<SocialNetworks />} />
           <Route path="/win-prize" element={<WinPrize />} />
+          <Route path="/articles" element={<Articles />} />
         </Routes>
       </main>
     </div>
