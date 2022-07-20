13a34ccff5fec848d90148f4df31482347a61947
Mirha Masala <mirha@hey.com>
2022-07-20 21:07:35 +0200

    Remove layout styles from component

--- frontend/src/components/Wheel/button/index.js
+++ frontend/src/components/Wheel/button/index.js
@@ -2,13 +2,14 @@ import { PropTypes } from "prop-types";
 import "./button.css";
 
 const Button = (props) => {
-
   return (
-    <div className="alignVertical">
-      <span className="spinButton" onClick="" style={{ "border": `3px solid ${props.colors[0]}` }}>
-        SPIN
-      </span>
-    </div>
+    <span
+      className="spinButton"
+      onClick=""
+      style={{ border: `3px solid ${props.colors[0]}` }}
+    >
+      SPIN
+    </span>
   );
 };
 
