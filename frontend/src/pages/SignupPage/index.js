7727253cddede8a1a6f723e2c30feba478a7de80
Mirha Masala <26620750+mirhamasala@users.noreply.github.com>
2022-07-26 08:51:06 +0200

    Replace PNGs with SVGs (#36)
    * Replace PNGs with SVGs
    
    * Remove PNG imports
    
    Co-authored-by: beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>

--- frontend/src/pages/SignupPage/index.js
+++ frontend/src/pages/SignupPage/index.js
@@ -1,6 +1,5 @@
 import Button from "components/Button";
 import Header from "components/Header";
-import SocialMediaButton from "components/SocialMediaButton";
 import Title from "components/Typography/Title";
 
 import styles from "./index.module.css";
@@ -11,11 +10,7 @@ const SignupPage = () => {
       <Header>Subvisual</Header>
       <Title>Join us to start spinning</Title>
 
-      <div className={styles.signup}>
-        <SocialMediaButton name="github" username="subvisual" />
-        <SocialMediaButton name="linkedin" username="wearesubvisual" />
-        <SocialMediaButton name="behance" username="subvisual" />
-      </div>
+      <div className={styles.signup}></div>
 
       <Button>Join Us</Button>
     </div>
