270795124071b4ec8a3fa8b19715f10e8dcfbb11
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-07-28 12:00:42 +0100

    CR

--- frontend/src/components/Wheel/index.js
+++ frontend/src/components/Wheel/index.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
 import styles from "./index.module.css";
-import "./index.css";
+import classNames from "classnames";
 
 import { WHEEL_CONFIG } from "constants/Subvisual.js";
+import PrizePage from "pages/PrizePage";
 
 const Wheel = () => {
   const [prizesInfo, setPrizesInfo] = useState([]);
-  const [rotate, setRotate] = useState("circle");
+  const [isRotating, setIsRotating] = useState(false);
+  const [isPrizePage, setIsPrizePage] = useState(false);
 
-  async function startRotate() {
-    setRotate("circle start-rotate");
+  function startRotate() {
+    setIsRotating(!isRotating);
+
     setTimeout(() => {
-      setRotate("circle start-rotate stop-rotate");
-    }, Math.floor(Math.random() * 10000) + 1);
+      setIsPrizePage(true);
+    }, 5000);
   }
 
   function wheelStyle(i) {
@@ -43,35 +46,47 @@ const Wheel = () => {
         setPrizesInfo(data);
       });
   }, []);
+
+  const circleClassName = classNames({
+    circle: true,
+    rotation: isRotating,
+  });
+
   return (
     <>
-      <h1 className={styles.center}>Prizes</h1>
-      <div className={styles.arrow}></div>
+      {!isPrizePage ? (
+        <>
+          <h1 className={styles.root}>Wheel</h1>
+          <div className={styles.arrow}></div>
 
-      <ul className={rotate}>
-        {prizesInfo.map((prize, index) => {
-          return (
-            <li
-              className={styles.prize}
-              key={prize?.id}
-              style={wheelStyle(index)}
-            >
-              <div contentEditable="true" spellCheck="false">
-                <span className={styles.text}>{prize?.name}</span>
-                <img
-                  src={prize?.image}
-                  alt={prize?.name}
-                  className={styles.img_prizes}
-                />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+          <ul className={circleClassName}>
+            {prizesInfo.map((prize, index) => {
+              return (
+                <li
+                  className={styles.prize}
+                  key={prize?.id}
+                  style={wheelStyle(index)}
+                >
+                  <div contentEditable="true" spellCheck="false">
+                    <span className={styles.text}>{prize?.name}</span>
+                    <img
+                      src={prize?.image}
+                      alt={prize?.name}
+                      className={styles.prizeImage}
+                    />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
 
-      <button className={styles.spin_button} onClick={startRotate}>
-        SPIN
-      </button>
+          <button className={styles.spinButton} onClick={startRotate}>
+            SPIN
+          </button>
+        </>
+      ) : (
+        <PrizePage />
+      )}
     </>
   );
 };
