88d0e79e4b1224f253a24809f9a78740dfd6c455
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-08-01 15:33:00 +0100

    add postgresql

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,12 +56,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-<<<<<<< HEAD
-=======
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
->>>>>>> Active Admin setup
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
