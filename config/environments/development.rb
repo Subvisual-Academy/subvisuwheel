baee5ca5abe7f9aaa580ad1a286db57172a81ef2
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-02 15:28:34 +0100

    Set up mailcatcher config
    Why:
    * The app will need a service to send email
    
    How:
    * Completing the development file in environments directory with information about the action mailer. Followed the recomendations from https://mailcatcher.me/ . This included not putting mailcatcher in Gemfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Mailcatcher configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
