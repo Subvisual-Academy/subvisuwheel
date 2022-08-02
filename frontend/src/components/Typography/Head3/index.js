51e757c8a14576fbd04e9dc8d77b4e65d5cf2c30
Joao Reis <joaofranciscoreis13@outlook.com>
2022-08-02 11:39:44 +0100

    Fix heading tag

--- frontend/src/components/Typography/Head3/index.js
+++ frontend/src/components/Typography/Head3/index.js
@@ -2,7 +2,7 @@ import { PropTypes } from "prop-types";
 import styles from "./index.module.css";
 
 const Head3 = ({ children }) => {
-  return <div className={styles.head3}>{children}</div>;
+  return <h3 className={styles.head3}>{children}</h3>;
 };
 
 Head3.propTypes = {
