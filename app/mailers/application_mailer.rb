0e64b6d732676549d336961f8700c545ba3dfe6c
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-08-03 18:15:56 +0100

    Fix rubocop problems

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "geral@subvisual.com"
+  default from: 'geral@subvisual.com'
   # layout 'win_merch'
 
   def win_prize_email
     mail(to: params[:email],
          subject: "(&#129297;) You won a Prize at Subvisual's wheel of fortune!",
          body: "Email test 7 ... name: #{params[:name]}",
-         content_type: "text/html")
+         content_type: 'text/html')
   end
 end
