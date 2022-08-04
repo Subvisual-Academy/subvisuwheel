9c7166f7056300f05f390001971f650545fab06d
Joao Reis <joaofranciscoreis13@outlook.com>
2022-08-04 15:06:34 +0100

    in progress

---
+++ frontend/src/components/Logo/index.js
@@ -0,0 +1,7 @@
+import styles from "./index.module.css";
+
+const Logo = () => {
+  return <span className={styles.logo}>Wheel of Fortune</span>;
+};
+
+export default Logo;
--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -1,4 +1,4 @@
-import Logo from "components/Typography/Logo";
+import Logo from "components/Logo";
 import Button from "components/Button";
 import Body from "components/Typography/Body";
 import Heading3 from "components/Typography/Heading3";
@@ -10,27 +10,37 @@ import styles from "./index.module.css";
 const HomePage = () => {
   return (
     <>
-      <div className={styles.svgtop}>
+      <div className={styles.svgWrapperTop}>
         <TopRight />
       </div>
-      <div className={styles.title}>
-        <Logo>Wheel of Fortune</Logo>
-      </div>
 
-      <div className={styles.heading}>
-        <Heading3>
-          An exclusive fortune wheel with prizes ranging from Subvisual merch to
-          NFTs*
-        </Heading3>
-      </div>
+      <div className={styles.root}>
+        <div>
+          <div className={styles.logoWrapper}>
+            <Logo />
+          </div>
 
-      <div className={styles.info}>
-        <Body>*Token-based prizes will require a digital wallet to claim.</Body>
-      </div>
+          <div className={styles.heading}>
+            <Heading3>
+              An exclusive fortune wheel with prizes ranging from Subvisual
+              merch to NFTs*
+            </Heading3>
+          </div>
 
-      <BotLeft />
-      <div className={styles.btn}>
-        <Button>Let&apos;s go</Button>
+          <div className={styles.info}>
+            <Body>
+              *Token-based prizes will require a digital wallet to claim.
+            </Body>
+          </div>
+
+          <div className={styles.svgWrapperBottom}>
+            <BotLeft />
+          </div>
+        </div>
+
+        <div className={styles.btn}>
+          <Button>Let&apos;s go</Button>
+        </div>
       </div>
     </>
   );
