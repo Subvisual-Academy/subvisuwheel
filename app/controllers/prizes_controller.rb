4c8d02327da2466a178cfc5534ad4a4e24561cff
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-08-04 15:35:05 +0100

    Fix more rubocop problems

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -11,6 +11,8 @@ class PrizesController < ApplicationController
     render json: prize
   end
 
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
   def win_prize
     prizes_list = Prize.all.each_with_object({}) do |prize, acc|
       acc[prize.name] = prize.percentage
@@ -18,17 +20,21 @@ class PrizesController < ApplicationController
 
     prize_won = Pickup.new(prizes_list).pick
     lead = Lead.find_by(email: params[:email])
-    prize = Prize.find_by(name: prize_won)
-    lead.update(prize: prize)
+    prize_info = Prize.find_by(name: prize_won)
+    lead.update(prize: prize_info)
 
-    if prize.prize_type == "Merch"
+    if prize_info.prize_type == 'Merch'
+      # rubocop:disable Layout/LineLength
       ApplicationMailer.with(email: lead.email, lead_name: lead.name, prize_name: prize_won, code_to_claim: '23345').win_merch_prize_email.deliver_now
     else
       ApplicationMailer.with(email: lead.email, lead_name: lead.name, prize_name: prize_won).win_nft_prize_email.deliver_now
+      # rubocop:enable Layout/LineLength
     end
 
     render json: Prize.find_by(name: prize_won)
   end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
   private
 
