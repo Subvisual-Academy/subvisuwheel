76e902f798b2534e638b7bbe0d1302b345bf79f9
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-05 13:13:23 +0100

    Add more props to input component
    Why:
    * In the purposes of doing the sign up page, noticed that it wouldn't be possible to get the information on the box without adding a few props
    
    How:
    * By adding onChange, value and an id to the input custom component

--- frontend/src/components/Input/index.js
+++ frontend/src/components/Input/index.js
@@ -2,7 +2,16 @@ import { PropTypes } from "prop-types";
 import styles from "./index.module.css";
 import classNames from "classnames";
 
-const Input = ({ type, placeholder, disabled, error, errorMessage }) => {
+const Input = ({
+  id,
+  type,
+  placeholder,
+  disabled,
+  error,
+  errorMessage,
+  value,
+  onChange,
+}) => {
   const inputClassNames = classNames(styles.input, {
     [styles.hasError]: error,
   });
@@ -11,21 +20,27 @@ const Input = ({ type, placeholder, disabled, error, errorMessage }) => {
     <>
       {error ? <p className={styles.errorMessage}>{errorMessage}</p> : null}
       <input
+        id={id}
         disabled={disabled}
         type={type}
         placeholder={placeholder}
         className={inputClassNames}
+        value={value}
+        onChange={onChange}
       />
     </>
   );
 };
 
 Input.propTypes = {
+  id: PropTypes.string.isRequired,
   type: PropTypes.oneOf(["text", "email"]).isRequired,
   placeholder: PropTypes.string.isRequired,
   errorMessage: PropTypes.string,
   disabled: PropTypes.bool,
   error: PropTypes.bool,
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
 };
 
 Input.defaultProps = {
