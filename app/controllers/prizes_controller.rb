0847382ef5172f7dfad106151cd6c6c4e4d2611b
Ricardo Vaz <55494046+RicardoVaz1@users.noreply.github.com>
2022-08-05 10:25:38 +0100

    Send email with the prize to the Lead (#67)
    Why:
    * https://3.basecamp.com/4319812/buckets/26581088/todos/5183373573
    
    How:
    * By following https://guides.rubyonrails.org/action_mailer_basics.html
    * Using Mailcatcher
    * Add win_prize name to use it on email
    * Add merch and nft emails templates

--- app/controllers/prizes_controller.rb
+++ app/controllers/prizes_controller.rb
@@ -12,23 +12,46 @@ class PrizesController < ApplicationController
   end
 
   def win_prize
-    prizes_list = Prize.all.each_with_object({}) do |prize, acc|
-      acc[prize.name] = prize.percentage
-    end
+    won_prize = sort_prize
+    name_lead = assign_prize_to_lead(won_prize)
+    send_email(name_lead, won_prize)
 
-    pickup = Pickup.new(prizes_list)
-    win_prize = pickup.pick
-
-    lead = Lead.where(email: params[:email])
-    prize_info = Prize.find_by(name: win_prize)
-    lead.update(prize: prize_info)
-
-    render json: Prize.find_by(name: win_prize)
+    render json: Prize.find_by(name: won_prize)
   end
 
   private
 
   def prize_params
     params.require(:prize).permit(:name, :percentage, :image, :prize_type, :description, :identifier)
+  end
+
+  def sort_prize
+    prizes_list = Prize.all.each_with_object({}) do |prize, acc|
+      acc[prize.name] = prize.percentage
+    end
+
+    Pickup.new(prizes_list).pick
+  end
+
+  def assign_prize_to_lead(prize_won)
+    lead = Lead.find_by(email: params[:email])
+    prize_info = Prize.find_by(name: prize_won)
+    lead.update(prize: prize_info)
+
+    lead.name
+  end
+
+  def send_email(name_lead, prize_won)
+    prize_info = Prize.find_by(name: prize_won)
+
+    if prize_info.prize_type == 'Merch'
+      ApplicationMailer.with(email: params[:email], lead_name: name_lead, prize_name: prize_won, code_to_claim: '23345')
+                       .win_merch_prize_email
+                       .deliver_now
+    else
+      ApplicationMailer.with(email: params[:email], lead_name: name_lead, prize_name: prize_won)
+                       .win_nft_prize_email
+                       .deliver_now
+    end
   end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,30 @@
 # frozen_string_literal: true
 
+# Send email to the Lead
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  default from: 'geral@subvisual.com'
+
+  def win_merch_prize_email
+    @lead_name = params[:lead_name]
+    @prize_name = params[:prize_name]
+    @code_to_claim = params[:code_to_claim]
+    @url = 'mailto:contact@subvisual.com?subject=Hi%40Subvisual!%20I%20want%20to%20talk%20to%20you'
+
+    mail(to: params[:email],
+         subject: "🤑 You won a Prize at Subvisual's wheel of fortune!",
+         template_path: 'win_prize',
+         template_name: 'win_merch')
+  end
+
+  def win_nft_prize_email
+    @lead_name = params[:lead_name]
+    @prize_name = params[:prize_name]
+    @form_link = 'https://forms.gle/PKEc4CGbFMbHAfuB8'
+    @url = 'mailto:contact@subvisual.com?subject=Hi%40Subvisual!%20I%20want%20to%20talk%20to%20you'
+
+    mail(to: params[:email],
+         subject: "🤑 You won a Prize at Subvisual's wheel of fortune!",
+         template_path: 'win_prize',
+         template_name: 'win_nft')
+  end
 end
