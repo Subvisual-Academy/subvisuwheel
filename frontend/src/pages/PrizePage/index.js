60ff1788ae6de06767881b0e4e8e31fb3b054773
Joao Reis <joaofranciscoreis13@outlook.com>
2022-08-08 11:53:28 +0100

    Style prize page

--- frontend/src/pages/PrizePage/index.js
+++ frontend/src/pages/PrizePage/index.js
@@ -1,27 +1,47 @@
-import { COMPANY } from "constants/Subvisual.js";
 import { PropTypes } from "prop-types";
 
-import Button from "components/Button";
-import Body from "components/Typography/Body";
 import Logo from "components/Logo";
+import Heading1 from "components/Typography/Heading1";
+import Heading2 from "components/Typography/Heading2";
+import Button from "components/Button";
+
+import { ReactComponent as WheelImageTopRight } from "assets/svgs/wheel-background/top-right.svg";
+import { ReactComponent as WheelImageBottom } from "assets/svgs/wheel-background/bottom.svg";
 
 import styles from "./index.module.css";
 
 const PrizePage = ({ prize }) => {
   return (
     <div className={styles.root}>
-      <Logo>{`Congrats, you won a ${prize.name}`}</Logo>
-
-      <div className={styles.prize}>
-        <img src={prize.image} alt={prize.name} />
+      <div className={styles.svgWrapperTop}>
+        <WheelImageTopRight />
       </div>
-      <Body>
-        {prize.is_merch
-          ? `Visit the ${COMPANY} booth to claim your prize`
-          : "Check your email to claim the NFT"}
-      </Body>
 
-      <Button>Finish</Button>
+      <div className={styles.main}>
+        <div className={styles.logoWrapper}>
+          <Logo />
+        </div>
+
+        <div className={styles.headingWrapper}>
+          <Heading1>You won a {prize.name}!</Heading1>
+        </div>
+
+        <div className={styles.bodyWrapper}>
+          <Heading2>
+            {prize.prize_type === "NFT"
+              ? "Check your email to confirm and claim your prize."
+              : "Check your email, then see a staff member to claim."}
+          </Heading2>
+        </div>
+      </div>
+
+      <div className={styles.svgWrapperBottom}>
+        <WheelImageBottom />
+      </div>
+
+      <div className={styles.btn}>
+        <Button href="/social-network">Finish</Button>
+      </div>
     </div>
   );
 };
@@ -29,5 +49,5 @@ const PrizePage = ({ prize }) => {
 export default PrizePage;
 
 PrizePage.propTypes = {
-  prize: PropTypes.isRequired,
+  prize: PropTypes.string,
 };
