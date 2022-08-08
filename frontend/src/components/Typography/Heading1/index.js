d47bf38190e6be5dbcf9296395bf284a187108bd
João Francisco Reis <86988151+jreis13@users.noreply.github.com>
2022-08-08 16:20:55 +0100

    Style prize page (#77)

--- frontend/src/components/Typography/Heading1/index.js
+++ frontend/src/components/Typography/Heading1/index.js
@@ -1,12 +1,12 @@
 import { PropTypes } from "prop-types";
 import styles from "./index.module.css";
 
-const Head1 = ({ children }) => {
-  return <h1 className={styles.head1}>{children}</h1>;
+const Heading1 = ({ children }) => {
+  return <h1 className={styles.heading1}>{children}</h1>;
 };
 
-Head1.propTypes = {
+Heading1.propTypes = {
   children: PropTypes.string.isRequired,
 };
 
-export default Head1;
+export default Heading1;
