d48a6b6b5e8bb0b3b9c4107fe1d89021a395f7f0
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-08-10 15:17:46 +0100

    Add image to emails
    Why:
    * https://3.basecamp.com/4319812/buckets/26581088/todos/5216059191
    
    How:
    * Using a img tag with the src getting the image from the public folder

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,13 +2,14 @@
 
 # Send email to the Lead
 class ApplicationMailer < ActionMailer::Base
-  default from: 'geral@subvisual.com'
+  default from: 'geral.subvisual@gmail.com'
 
   def win_merch_prize_email
     @lead_name = params[:lead_name]
     @prize_name = params[:prize_name]
     @code_to_claim = params[:code_to_claim]
     @url = 'mailto:contact@subvisual.com?subject=Hi%40Subvisual!%20I%20want%20to%20talk%20to%20you'
+    @server_url = ENV.fetch('SERVER_URL')
 
     mail(to: params[:email],
          subject: "🤑 You won a Prize at Subvisual's wheel of fortune!",
@@ -21,6 +22,7 @@ class ApplicationMailer < ActionMailer::Base
     @prize_name = params[:prize_name]
     @form_link = 'https://forms.gle/PKEc4CGbFMbHAfuB8'
     @url = 'mailto:contact@subvisual.com?subject=Hi%40Subvisual!%20I%20want%20to%20talk%20to%20you'
+    @server_url = ENV.fetch('SERVER_URL')
 
     mail(to: params[:email],
          subject: "🤑 You won a Prize at Subvisual's wheel of fortune!",
