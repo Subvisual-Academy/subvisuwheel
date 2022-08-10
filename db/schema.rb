8d3a99d051282bc39d457d2e42109a15110e5cb3
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-10 16:23:08 +0100

    CR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_05_102451) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_10_121719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,10 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_05_102451) do
   create_table "leads", force: :cascade do |t|
     t.string "name"
     t.string "email"
-<<<<<<< Updated upstream
-    t.string "interests"
-=======
->>>>>>> Stashed changes
     t.boolean "data_proc_consent", default: false
     t.boolean "delivered", default: false
     t.datetime "created_at", null: false
