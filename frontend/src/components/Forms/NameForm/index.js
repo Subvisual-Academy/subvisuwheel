df39134be048929f92bc9e6ae0e561bb1b4d3d75
Mirha Masala <mirha@hey.com>
2022-08-11 20:33:26 +0200

    Refactor forms

--- frontend/src/components/Forms/NameForm/index.js
+++ frontend/src/components/Forms/NameForm/index.js
@@ -1,38 +1,31 @@
 import { PropTypes } from "prop-types";
 
-import Heading2 from "components/Typography/Heading2";
-import Heading3 from "components/Typography/Heading3";
+import FormLayout from "components/Forms/FormLayout";
 import Input from "components/Input";
 
-import styles from "./index.module.css";
-
-const NameForm = ({ name, handleChange, error }) => {
+const NameForm = ({ error, handleChange, name }) => {
   return (
-    <>
-      <div className={styles.header}>
-        <Heading2>Just a few questions...</Heading2>
-      </div>
-      <div className={styles.prompt}>
-        <Heading3>What&apos;s your name?</Heading3>
-      </div>
-      <div className={styles.input}>
-        <Input
-          id="name"
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={handleChange("name")}
-          error={error.hasError}
-          errorMessage={error.message}
-        />
-      </div>
-    </>
+    <FormLayout
+      headerText="Just a few questions..."
+      promptText="What's your name?"
+    >
+      <Input
+        id="name"
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={handleChange("name")}
+        error={error.hasError}
+        errorMessage={error.message}
+      />
+    </FormLayout>
   );
 };
+
 NameForm.propTypes = {
-  name: PropTypes.string.isRequired,
-  handleChange: PropTypes.func.isRequired,
   error: PropTypes.object,
+  handleChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default NameForm;
