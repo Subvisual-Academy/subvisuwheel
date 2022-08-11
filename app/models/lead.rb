a32da780f324a6d290e205a1ec465aa320954a36
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-11 12:33:12 +0100

    Add lead model to back office
    Why:
    * Admin should be able to access,edit and delete lead information as well as export it
    
    How:
    * By generating with bin/rails generate active_admin:resource Lead

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Lead < ApplicationRecord
-  validates :name, :email, :interests, presence: true
+  validates :name, :email, presence: true
   belongs_to :prize, optional: true
 end
