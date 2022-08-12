a99212306ea789b0d5094c73abd6e1ffa3c09edc
Mirha Masala <mirha@hey.com>
2022-08-12 12:32:33 +0200

    Remove fragment

--- frontend/src/components/Checkbox/index.js
+++ frontend/src/components/Checkbox/index.js
@@ -1,38 +1,32 @@
 import { PropTypes } from "prop-types";
 
-import styles from "./index.module.css";
+import styles from "./index.module.scss";
 
 function Checkbox({ id, label, disabled, onChange }) {
   return (
-    <>
-      <div className={styles.inputWrapper}>
-        <input
-          id={id}
-          className={styles.input}
-          type="checkbox"
-          disabled={disabled}
-          onChange={onChange}
-        />
-        <label htmlFor={id} className={styles.label}>
-          {label}
-        </label>
-        <span className={styles.control}>
-          <svg
-            viewBox="0 0 10 8"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M1 4L4 6.66667L9 1.33333"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </span>
-      </div>
-    </>
+    <div className={styles.inputWrapper}>
+      <input
+        id={id}
+        className={styles.input}
+        type="checkbox"
+        disabled={disabled}
+        onChange={onChange}
+      />
+      <label htmlFor={id} className={styles.label}>
+        {label}
+      </label>
+      <span className={styles.control}>
+        <svg viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M1 4L4 6.66667L9 1.33333"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </span>
+    </div>
   );
 }
 
