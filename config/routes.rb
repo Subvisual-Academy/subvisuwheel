297a7a3449f264f6c6c07fd49c90a7eb77e498cf
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-08-16 10:40:16 +0100

    Block GET Leads route
    Why:
    * https://3.basecamp.com/4319812/buckets/26581088/todos/5225150075
    
    How:
    * Deleting the GET Leads route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   get '/prizes', to: 'prizes#index'
   post '/prizes', to: 'prizes#create'
   post '/win-prize', to: 'prizes#win_prize'
-  get '/leads', to: 'leads#index'
   post '/leads', to: 'leads#create'
 end
