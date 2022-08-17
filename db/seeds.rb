c15f68d063e4c8365d9f83f7415624a93c741c20
RicardoVaz1 <vaz.ric4rd0@gmail.com>
2022-08-17 16:09:17 +0100

    Creating Email seeds
    Why:
    * To have the template in the Admin Backoffice
    
    How:
    * Creating Email seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,53 @@ Social.create(name: "Twitter", url: "https://twitter.com/subvisual", company_id:
 Social.create(name: "Instagram", url: "https://instagram.com/wearesubvisual", company_id: 1)
 Social.create(name: "LinkedIn", url: "https://linkedin.com/subvisual", company_id: 1)
 Social.create(name: "Facebook", url: "https://facebook.com/subvisual.co", company_id: 1)
+
+Email.create(email_type: 'Merch', subject: '🤑 You won a Merch Prize at Subvisual\'s wheel of fortune!', 
+  body: '<img src="<%= @server_url %>logo-wheel.png" alt="Logo Wheel">
+
+  <p>Hi <%= @lead_name %>,</p>
+
+  <p>Congratulations for winning <%= @prize_name %>!</p>
+
+  <p>
+    Ready to claim it? 🚀 <br />
+    show the subvisual staff this code: <%= @code_to_claim %> and receive your prize right away.
+  </p>
+
+  <p>
+    Can\'t do it now? 🤔 <br />
+    That\'s a bummer, but you can still get your prize. <br />
+    All you need to do is answer this email and we will get back to you as soon as we can! (name, address, phone number)
+  </p>
+
+  <br />
+
+  <p>
+    Thank you for playing with Subvisual - the place where digital products and ventures go further. <br />
+    Would you like to get to know us better? <br />
+    <a href="<%= @url %>" target="_blank">Let\'s talk now.</a>
+  </p>')
+
+Email.create(email_type: 'NFT', subject: '🤑 You won an NFT Prize at Subvisual\'s wheel of fortune!',
+  body: '<img src="<%= @server_url %>logo-wheel.png" alt="Logo Wheel">
+
+  <p>Hi <%= @lead_name %>,</p>
+
+  <p>Congratulations for winning <%= @prize_name %>!</p>
+
+  <p>
+    Ready to claim it? 🚀 <br />
+    <b>
+      Insert your digital wallet address in this 
+      <a href="<%= @form_link %>" target="_blank">form</a>
+      to collect your prize.
+    </b>
+  </p>
+
+  <br />
+
+  <p>
+    Thank you for playing with Subvisual - the place where digital products and ventures go further. <br />
+    Would you like to get to know us better? <br />
+    <a href="<%= @url %>" target="_blank">Let\'s talk now.</a>
+  </p>')
