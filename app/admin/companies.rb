ef7fd93d2164afc92f7694a327e23b430f5de5ba
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-19 14:51:10 +0100

    Fix delete company foreign key dependency
    Why:
    * It wasn't possible to delete a company if it has socials associated because of the foreign key
    
    How:
    * By removing the socials in the active admin controller before removing the company

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -2,4 +2,11 @@
 
 ActiveAdmin.register Company do
   permit_params :name, :website, :selected
+
+  controller do
+    def destroy
+      Social.where(company_id: params[:id]).destroy_all
+      super
+    end
+  end
 end
