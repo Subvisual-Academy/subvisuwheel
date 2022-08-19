96a4380d4ecf3db435fa84542fbaa13d59f25d0a
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-19 10:06:26 +0100

    CR

--- app/controllers/terms_and_conditions_controller.rb
+++ app/controllers/terms_and_conditions_controller.rb
@@ -3,6 +3,10 @@
 # Controller that manages terms and conditions
 class TermsAndConditionsController < ApiController
   def index
+    render json: TermsAndCondition.all
+  end
+
+  def show
     render json: TermsAndCondition.find(1).slice(:link)
   end
 end
