79ce8873a487b2135ea45c62c400108b7be256d3
Ricardo Vaz <55494046+RicardoVaz1@users.noreply.github.com>
2022-08-19 10:44:52 +0100

    Fix Prizes validation is above 100% (#141)
    Why:
    * https://3.basecamp.com/4319812/buckets/26581088/todos/5238453637
    
    How:
    * Creating a validation in the Admin Prizes controller

--- app/admin/prizes.rb
+++ app/admin/prizes.rb
@@ -2,4 +2,33 @@
 
 ActiveAdmin.register Prize do
   permit_params :name, :percentage, :image, :description, :identifier, :prize_type
+
+  controller do
+    def create
+      total_prizes_percentages = Prize.pluck(:percentage).sum
+      if total_prizes_percentages + params[:prize][:percentage].to_i > 100
+        render json: 'Error! The total percentage is greater than 100%!'
+      else
+        Prize.create(prize_params)
+        redirect_to '/admin/prizes'
+      end
+    end
+
+    def update
+      prize_info = Prize.find_by(id: params[:id])
+      total_prizes_percentages = Prize.pluck(:percentage).sum - prize_info.percentage
+
+      if total_prizes_percentages + params[:prize][:percentage].to_i > 100
+        render json: 'Error! The total percentage is greater than 100%!'
+      else
+        super
+      end
+    end
+
+    private
+
+    def prize_params
+      params.require(:prize).permit(:name, :percentage, :image, :prize_type, :description, :identifier)
+    end
+  end
 end
