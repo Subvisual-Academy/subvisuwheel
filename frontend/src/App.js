399ef6a158b3b59e7fa16bf50c26795cb6b1e582
Beatriz Rodrigues <62039765+beasrodrigues24@users.noreply.github.com>
2022-08-22 14:00:06 +0100

    Close random access to /wheel and /win-prize (#137)
    Why:
    * The user shouldn't be able to access these pages if he doesn't do the flow correctly because he'll have blank error screens
    
    How:
    * By redirecting the user back to the home screen with useEffect hook that validates if the local storage suggests the user has played
    * By doing this in two components, one for the Wheel (checks if the email exists) and one for the prize (checks if the user has won a prize)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 
+import ValidateEmail from "components/ValidateEmail";
+import ValidatePrize from "components/ValidatePrize";
+
 import HomePage from "pages/HomePage";
 import PrizePage from "pages/PrizePage";
 import SignupPage from "pages/SignupPage";
@@ -13,9 +16,23 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/wheel" element={<WheelPage />} />
+        <Route
+          path="/wheel"
+          element={
+            <ValidateEmail>
+              <WheelPage />
+            </ValidateEmail>
+          }
+        />
         <Route path="/social-network" element={<SocialPage />} />
-        <Route path="/win-prize" element={<PrizePage />} />
+        <Route
+          path="/win-prize"
+          element={
+            <ValidatePrize>
+              <PrizePage />
+            </ValidatePrize>
+          }
+        />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </>
---
+++ frontend/src/components/ValidateEmail/index.js
@@ -0,0 +1,24 @@
+import { PropTypes } from "prop-types";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const ValidatePrize = ({ children }) => {
+  const navigator = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("userData")) {
+      setLoading(true);
+    } else {
+      navigator("/");
+    }
+  }, [navigator]);
+
+  return loading ? children : null;
+};
+
+ValidatePrize.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ValidatePrize;
