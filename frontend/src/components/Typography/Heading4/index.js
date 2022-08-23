d2229c3fe41bd1e374939b664cb3de2211187374
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-23 14:37:23 +0100

    Run linters and CR

--- frontend/src/components/Typography/Heading4/index.js
+++ frontend/src/components/Typography/Heading4/index.js
@@ -2,7 +2,7 @@ import { PropTypes } from "prop-types";
 import styles from "./index.module.css";
 
 const Heading4 = ({ children }) => {
-  return <h3 className={styles.heading4}>{children}</h3>;
+  return <h4 className={styles.heading4}>{children}</h4>;
 };
 
 Heading4.propTypes = {
