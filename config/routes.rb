56dd7e4177c67b8e5939b7a6976b1acb382593ae
beasrodrigues24 <beatrizsilvarodrigues12@gmail.com>
2022-08-24 14:49:34 +0100

    Adapt routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/prizes', to: 'prizes#index'
   post '/prizes', to: 'prizes#create'
   get '/socials', to: 'socials#show'
-  post '/prize', to: 'prizes#win_prize'
+  patch '/leads/', to: 'leads#update'
   post '/leads', to: 'leads#create'
   get '/emails', to: 'emails#index'
   get '/terms', to: 'terms_and_conditions#show'
