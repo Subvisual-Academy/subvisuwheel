7ec4ed26534022601fb5efefc40a31ca3291533b
Mirha Masala <mirha@hey.com>
2022-08-25 16:39:25 +0200

    Redo PrizePage

--- frontend/src/pages/PrizePage/index.js
+++ frontend/src/pages/PrizePage/index.js
@@ -1,7 +1,8 @@
 import Heading from "components/Typography/Heading";
 import Body from "components/Typography/Body";
 import Button from "components/Button";
-import LayoutWithBackground from "components/LayoutWithBackground";
+import { ReactComponent as WheelImageBottomLeft } from "assets/svgs/wheel-background/bottom-left-landing.svg";
+import { ReactComponent as WheelImageTopRight } from "assets/svgs/wheel-background/top-right.svg";
 
 import styles from "./index.module.css";
 
@@ -9,20 +10,28 @@ const PrizePage = () => {
   const prize = JSON.parse(localStorage.getItem("prizeWon"));
 
   return (
-    <LayoutWithBackground isInvertedBottom={true}>
-      <div className={styles.root}>
+    <div className={styles.root}>
+      <div className={styles.wheelTop}>
+        <WheelImageTopRight />
+      </div>
+      <div className={styles.container}>
         <div className={styles.main}>
-          <Heading>Congratulations!</Heading>
-          <Heading level={2}>You won: {prize.name}</Heading>
-          <Body>
-            {prize.type === "NFT"
-              ? "Check your email to confirm and claim your prize."
-              : "Check your email to claim your prize."}
-          </Body>
+          <div className={styles.content}>
+            <Heading>Congratulations!</Heading>
+            <Heading level={2}>You won: {prize.name}</Heading>
+            <Body>
+              {prize.type === "NFT"
+                ? "Check your email to confirm and claim your prize."
+                : "Check your email to claim your prize."}
+            </Body>
+          </div>
         </div>
         <Button href="/social">Finish</Button>
       </div>
-    </LayoutWithBackground>
+      <div className={styles.wheelBottom}>
+        <WheelImageBottomLeft />
+      </div>
+    </div>
   );
 };
 
